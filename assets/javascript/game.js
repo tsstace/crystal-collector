e5464a8758224bc49d9e818a8ef42a9e9264f7e0
Stacey Starken <staceystarken@gmail.com>
2018-05-11 21:32:10 -0500

    updated counter functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,81 @@
   var crystals = $("#crystals");
 
-    var crystalArray = [
-      "crystal1",
-      "crystal2",
-      "crystal3",
-      "crystal4"
-    ]
+  var crystalArray = [
+    "assets/images/citrine-3201605__480.jpg",
+    "assets/images/precious-stone-3259727__480.jpg",
+    "assets/images/colored-glass-3092416__480.jpg",
+    "assets/images/rock-crystal-1603480__480.jpg"
+  ]
 
-    var crystalValues = [];
-    var randomNumber;
-    var targetNumber;
-    var counter = 0;
-    var lossCount = 0;
-    var winCount = 0;
+  var crystalValues = [];
+  var randomNumber;
+  var targetNumber;
+  var counter = 0;
+  var lossCount = 0;
+  var winCount = 0;
+  var storeCrystalValue = '  ';
 
-    function pushRandomNumber() {
-      randomNumber = Math.floor(Math.random() * 12)
-      if (crystalValues.indexOf(randomNumber) === -1) {
-        crystalValues.push(randomNumber);
-      }
-      else {
-        pushRandomNumber();
-      }
+  function pushRandomNumber() {
+    randomNumber = Math.floor(Math.random() * (12 - 1) + 1);
+    if (crystalValues.indexOf(randomNumber) === -1) {
+      crystalValues.push(randomNumber);
+    } else {
+      pushRandomNumber();
     }
+  }
 
-    function createCrystals() {
-      for (var i = 0; i < 4; i++) {
-        pushRandomNumber();
-        $("#container").append(`
-        <img src=${crystalArray[i]} value=${randomNumber} />
+  function createCrystals() {
+    $("#crystal-images").html("");
+    for (var i = 0; i < 4; i++) {
+      pushRandomNumber();
+
+      $("#crystal-images").append(`
+        <img id="crystal${i+1}" class="crystal" src=${crystalArray[i]} value=${randomNumber} />
       `)
+    }
+  }
 
-        // Need to grab the array value of the crystal selected so it can be referenced
-        //    crystals.attr("data-crystalvalue", crystalValues[i]);
-        crystals.attr("data-crystalvalue", crystalValues[i]);
-      }
+  function getRandomInt(min, max) {
+    targetNumber = Math.floor(Math.random() * 102) + 19;
+    $("#number-to-guess").text(targetNumber);
+  }
+
+  function startGame() {
+    counter = 0;
+    $("#counter").text(counter);
+    createCrystals();
+    getRandomInt();
+  }
+
+  startGame();
+
+  $(document).on("click", ".crystal", function () {
+
+    var value = parseInt($(this).attr("value"));
+
+    counter += value;
+    console.log(counter);
+    $("#counter").text(counter);
+
+
+    //      Take the point value for the crystal selected and parse it so we can do math on it
+    //      var crystalValue = ($(this).attr("data-crystalvalue"));
+    //      crystalValue = parseInt(crystalArray[i]);
+    //      console.log(crystalValue);
+
+    // counter += crystalValues;
+    // $("#counter").text(counter);
+
+    if (counter === targetNumber) {
+      alert("You win!");
+      winCount++;
+      $("#user-wins").text(winCount);
+      startGame();
+    } else if (counter > targetNumber) {
+      alert("You lose!!");
+      lossCount++;
+      $("#user-losses").text(lossCount);
+      startGame();
     }
 
-    function getRandomInt(min, max) {
-      targetNumber = Math.floor(Math.random() * 102) + 19;
-      $("#number-to-guess").text(targetNumber);
-    }
-
-    function startGame() {
-      createCrystals();
-      getRandomInt();
-    }
-
-    startGame();
-
-    $(document).on("click", "#crystal-images", function () {
-      console.log(crystalValues);
-
-//      Take the point value for the crystal selected and parse it so we can do math on it
-//      var crystalValue = ($(this).attr("data-crystalvalue"));
-//      crystalValue = parseInt(crystalArray[i]);
-//      console.log(crystalValue);
-
-      counter += crystalValues;
-      $("#counter").text(counter);
-
-      if (counter === targetNumber) {
-        alert("You win!");
-        winCount++;
-        $("#user-wins").text(winCount);
-      }
-
-      else if (counter >= targetNumber) {
-        alert("You lose!!");
-        lossCount++;
-        $("#user-losses").text(lossCount);
-        startGame();
-      }
-
-    });
+  });
